5ca66fc856df83df34c3f96eb5c6a3643c0b52f2
zoriespay <enakoyabli@gmail.com>
2026-06-22 10:10:14 +0700

    Create Hero.tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>
+        Recurring Payments &
+        Payroll on Solana
+      </h1>
+
+      <p>
+        Approve once, let it run forever.
+      </p>
+    </section>
+  );
+}
